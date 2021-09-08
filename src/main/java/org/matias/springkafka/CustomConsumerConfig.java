c71e6a21ea055e1a6dd629a850922d9fa1e2f2a2
Matias Cascallares <matiascas@gmail.com>
2021-09-08 15:36:33 +0200

    Update CustomConsumerConfig.java

--- src/main/java/org/matias/springkafka/CustomConsumerConfig.java
+++ src/main/java/org/matias/springkafka/CustomConsumerConfig.java
@@ -23,12 +23,9 @@ class CustomConsumerConfig {
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
     }
 
